a69c6109169bfe1e31b143ea45d9bd4896fb89c5
WinerGero <korykin.27@mail.ru>
2023-03-13 22:59:54 +0200

    added functions: input_path, read_pdf added checking if

--- main.py
+++ main.py
@@ -3,8 +3,23 @@ python 3.11.2, sys 3.11.2, PyPDF2 3.0.1, py-anki 0.0.6, opencv-python 4.7.0.72
 """
 
 import sys
-import PyPDF2
+from PyPDF2 import PdfReader
 from py_anki import AnkiApi
 import cv2
 
 
+def input_path():
+    path = input("Укажите путь до PDF файла: ")
+    path = "Magaziny_vokabulyar.pdf"
+    return path
+
+
+def read_pdf(path):
+    reader = PdfReader(path)
+    page = reader.pages[0]
+    print(page.extract_text())
+
+
+if __name__ == "__main__":
+    path = input_path()
+    read_pdf(path)
